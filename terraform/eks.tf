0e1522f6847c94a19e4b4f791f73367bfe58d978
MaheshBabuk23 <maheshbabuk981@gmail.com>
2025-12-29 17:37:03 +0530

    Update EKS module configuration and AMI type

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,6 +1,7 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
+  cluster_version = "1.32"
 
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
@@ -23,11 +24,12 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-  ami_type       = "AL2023_x86_64_STANDARD"
-  instance_types = ["m5.large"]
+    ami_type       = "AL2_x86_64"
+    instance_types = ["m5.large"]
 
-  attach_cluster_primary_security_group = true
-}
+    attach_cluster_primary_security_group = true
+  }
+
 
   eks_managed_node_groups = {
       cluster-wg = {
